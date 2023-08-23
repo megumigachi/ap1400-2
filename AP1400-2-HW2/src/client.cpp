8225ce80cbd04c2ff4630297e1968f9bca3b8f92
megumingachi <472014574@qq.com>
2023-08-23 16:22:22 +0800

    823

--- AP1400-2-HW2/src/client.cpp
+++ AP1400-2-HW2/src/client.cpp
@@ -4,12 +4,15 @@ using std::string;
 
 Client::Client(string id, const Server &server)
     : id(id), server(&server)
-{}
+{
+}
 
-string Client::get_id(){
+string Client::get_id()
+{
     return this->id;
 }
 
-double Client::get_wallet(){
-    return ((Server*)(this->server))->get_wallet(this->id);
+double Client::get_wallet()
+{
+    return ((Server *)(this->server))->get_wallet(this->id);
 }
--- AP1400-2-HW2/src/server.cpp
+++ AP1400-2-HW2/src/server.cpp
@@ -1,55 +1,54 @@
 #include "server.h"
 #include <iostream>
-#include<random>
-
+#include <random>
 
 using std::make_shared;
 using std::shared_ptr;
 using std::string;
 
-
-string addRandomString(string origin){
+string addRandomString(string origin)
+{
     std::random_device rd;
     std::mt19937 generator(rd());
-    std::uniform_int_distribution<int> distribution(0,10);
-    string append="";
+    std::uniform_int_distribution<int> distribution(0, 10);
+    string append = "";
     for (size_t i = 0; i < 4; i++)
     {
-        append+=distribution(generator);
+        append += distribution(generator);
     }
-    return origin+append;
+    return origin + append;
 }
 
 Server::Server() {}
 
 shared_ptr<Client> Server::add_client(string id)
 {
-    bool exist=false;
+    bool exist = false;
     for (auto &pair : this->clients)
     {
-        string _id=pair.first->get_id();
-        if (id==_id)
+        string _id = pair.first->get_id();
+        if (id == _id)
         {
-            exist=true;
+            exist = true;
             break;
         }
     }
     shared_ptr<Client> c;
     if (exist)
     {
-        id=addRandomString(id);
+        id = addRandomString(id);
     }
-    c =make_shared<Client>(id,*this);
-     this->clients[c]=5;
-     return c;
+    c = make_shared<Client>(id, *this);
+    this->clients[c] = 5;
+    return c;
 }
 
-
-shared_ptr<Client>Server::get_client(string id){
-    shared_ptr<Client>c=nullptr;
+shared_ptr<Client> Server::get_client(string id)
+{
+    shared_ptr<Client> c = nullptr;
     for (auto &pair : this->clients)
     {
-        if (pair.first->get_id()==id)
+        if (pair.first->get_id() == id)
         {
             return pair.first;
         }
@@ -57,15 +56,14 @@ shared_ptr<Client>Server::get_client(string id){
     return nullptr;
 }
 
-double Server::get_wallet(std::string id){
-    auto c=this->get_client(id);
-    if (c!=nullptr)
+double Server::get_wallet(std::string id)
+{
+    auto c = this->get_client(id);
+    if (c != nullptr)
     {
         return this->clients[c];
     }
     return 0;
-    
 }
-
 
 // Server
